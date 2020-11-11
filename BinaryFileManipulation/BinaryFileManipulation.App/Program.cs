432061876737e113abb57b09890507b9ab9569a9
Chris Harrison <cah@stpaulsschool.org.uk>
2020-11-11 15:19:47 +0000

    Create the basic visual studio solution

---
+++ BinaryFileManipulation/BinaryFileManipulation.App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryFileManipulation.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello binary files");
+        }
+    }
+}
