e58fa373e291bc493d63cc31ce3312f2f16ac3bb
Chris Harrison <cah@stpaulsschool.org.uk>
2020-11-11 15:00:52 +0000

    Initial creation of the VS solution

---
+++ BinaryFileHandling/BinaryFileHandling.App/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace BinaryFileHandling.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] logo = File.ReadAllBytes("sps.bmp");
+            byte first = logo[0];
+            byte second = logo[1];
+
+            char b = 'B';
+            char m = 'M';
+
+            int res1 = first ^ (byte)b;
+            int res2 = second ^ (byte)m;
+        }
+    }
+}
